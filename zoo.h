1fdf04ee9a7dfd18666bf9ee57ee9625f8b93468
yottabyte9 <andrewcn@umich.edu>
2023-11-28 18:39:11 -0500

    Update zoo.h

--- zoo.h
+++ zoo.h
@@ -1,6 +1,21 @@
+// Project UID 3E33912F8BAA7542FC4A1585D2DB6FE0312725B9
+
 #include <cmath>
 #include <limits>
+#include <queue>
+
 using namespace std;
+
+struct MinHeapNode {
+    long vertex;
+    double distance;
+};
+
+struct Compare {
+    bool operator()(const MinHeapNode& lhs, const MinHeapNode& rhs) {
+        return lhs.distance > rhs.distance;
+    }
+};
 
 struct Coordinate {
 	bool visited;
@@ -10,9 +25,10 @@ struct Coordinate {
     long y;
     long index;
     long prevcoord;
+    char type;
     
-    Coordinate(long index_in, long x_in, long y_in)
-        : x(x_in), y(y_in), index(index_in){
+    Coordinate(long index_in, long x_in, long y_in, char type_in)
+        : x(x_in), y(y_in), index(index_in), type(type_in){
         visited = false;
         distance = numeric_limits<double>::infinity();
         prevcoord = -1;
